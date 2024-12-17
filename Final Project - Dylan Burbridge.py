5a4b22cfbd8dcd194c3a57989260da434544c57d
CapBurb <dylan.burbridge@maine.edu>
2024-12-17 15:23:30 -0500

    Create Final Project - Dylan Burbridge.py

---
+++ Final Project - Dylan Burbridge.py
@@ -0,0 +1,5 @@
+print("|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|")
+print("♣-♦-♥-♠- Welcome to Python BlackJack -♠-♥-♦-♣")
+print("|-------------------------------------------|")
+cardsuits = ['Diamonds', 'Hearts','Clubs','Spades']
+cardvalues = [ 'One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
