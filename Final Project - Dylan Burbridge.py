22e865222eb9d2fa647b12d25465b98c80a4b0fc
CapBurb <dylan.burbridge@maine.edu>
2024-12-18 12:17:06 -0500

    Update Final Project - Dylan Burbridge.py

--- Final Project - Dylan Burbridge.py
+++ Final Project - Dylan Burbridge.py
@@ -3,3 +3,8 @@ print("♣-♦-♥-♠- Welcome to Python BlackJack -♠-♥-♦-♣")
 print("|-------------------------------------------|")
 cardsuits = ['Diamonds', 'Hearts','Clubs','Spades']
 cardvalues = [ 'One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
+
+createdeck = [f"{value} of {suit}" for suit in cardsuits for value in cardvalues]
+
+print("\n create deck:")  
+for card in deck
