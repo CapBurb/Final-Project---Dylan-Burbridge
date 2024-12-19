bd41a5f1380e0516e6de4393ef9c0a4f848d22c4
CapBurb <dylan.burbridge@maine.edu>
2024-12-19 15:09:14 -0500

    Update Card_value_file.py

--- Card_value_file.py
+++ Card_value_file.py
@@ -2,7 +2,7 @@
 import random
 
 # This code turns the text value of cards into the values they respresent in blackjack, where cards are valued at 1-10, face cards at 10 and aces high at 11
-def playerhandvalue: 
+def playerhandvalue(playerhand): 
     cardvalues = {
         'One' : 1, 'Two' : 2, 'Three' : 3, 'Four' : 4, 'Five' : 5, 'Six' : 6, 'Seven' : 7, 'Eight' : 8, 'Nine' : 9, 'Ten' : 10, 'Jack' : 10, 'Queen' : 10, 'King' : 10, 'Ace' : 11 }
 
