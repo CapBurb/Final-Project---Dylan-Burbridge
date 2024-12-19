59a9cc3fc834e59f705906740c4be5ceef2234cf
CapBurb <dylan.burbridge@maine.edu>
2024-12-19 15:15:42 -0500

    Create Final_Project - Dylan Burbridge.py
    renamed file to include "_"

---
+++ Final_Project - Dylan Burbridge.py
@@ -0,0 +1,14 @@
+# This is the main file in which the player will interact with the computer in a player vs. house (computer) game of blackjack
+# The player will compete to get the highest amount of "Chips" which is a currency they can bet with
+# To motivate the player, the goal of this game is to get the highest amount of "Chips" this will be the players highscore
+# The game will end once the player runs out of "Chips"
+
+print("|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|")
+print("♣-♦-♥-♠- Welcome to Python BlackJack -♠-♥-♦-♣")
+print("|-------------------------------------------|")
+
+import random
+
+from Card_value_file import playerhandvalue
+from Final project card generation import createdeck
+
