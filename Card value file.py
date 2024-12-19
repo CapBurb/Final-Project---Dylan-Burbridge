df5c9c5a4544aaae50839a0a2d524b04a7274a26
CapBurb <dylan.burbridge@maine.edu>
2024-12-19 14:38:05 -0500

    Update Card value file.py
    This file will be used to calculate the value of the players and house cards and determine if aces are high or low

---
+++ Card value file.py
@@ -0,0 +1,8 @@
+# This line brings in a random value, needed for a "Shuffled" deck to be used
+import random
+
+# This code turns the text value of cards into the values they respresent in blackjack, where cards are valued at 1-10, face cards at 10 and aces high at 11
+def playerhandvalue: 
+    values = {
+        'One' = 1, 'Two' = 2, 'Three' = 3, 'Four' = 4, 'Five' = 5, 'Six' = 6, 'Seven' = 7, 'Eight' = 8, 'Nine' = 9, 'Ten' = 10, 'Jack' = 10, 'Queen' = 10, 'King' = 10, 'Ace' = 11 }
+
