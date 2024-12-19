ee3dcd8655dd5e82c6f9a92eb59b83bf7a87f88a
CapBurb <dylan.burbridge@maine.edu>
2024-12-19 13:50:40 -0500

    Update Final project card generation.py
    Added descriptions of code functions, and stopped testing code from being used by other files

--- Final project card generation.py
+++ Final project card generation.py
@@ -1,11 +1,18 @@
 print("|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|")
 print("♣-♦-♥-♠- Welcome to Python BlackJack -♠-♥-♦-♣")
 print("|-------------------------------------------|")
+
+# this section of code creates two variables, the cardsuits which coorespond to the four suits of a deck
+# and the cardvalues from one to ten as well as four face cards 
 cardsuits = ['Diamonds', 'Hearts','Clubs','Spades']
 cardvalues = [ 'One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
 
+
+
+# this line pairs the cardsuits to the cardvalues to create four sets of 14 unique cards for a deck total of 52 cards (no jokers)
 createdeck = [f"{value} of {suit}" for suit in cardsuits for value in cardvalues]
 
+#this line is used only to confirm that all 52 cards are present and not repeated, it will not be used outside of this file
 print("\n create deck:")  
 for card in createdeck:
     print(card)
