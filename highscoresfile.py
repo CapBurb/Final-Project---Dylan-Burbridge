e363255754d82a4bd229b2cb6402398520cdbefc
CapBurb <dylan.burbridge@maine.edu>
2024-12-20 16:20:38 -0500

    Highscore files
    these files are being used to track a players high score, it will use dictionaries and write to/read from a text file named "highscore.txt"

--- highscoresfile.py
+++ highscoresfile.py
@@ -1,1 +1,12 @@
-# this file will work with a text file named "highscore.txt"
+# this file will work with a text file named "highscore.txt"it will be used to 
+# track and save the ammount of chips each player has and use it to create
+# a list of top chip amounts, serving as a highscore board
+
+highscorefile = "highscores.txt"
+
+# this code will create a new variable that reads a highscore to players after
+# they quit the game or run out of chips
+# this will use a dictionarie to track a players chips, wins, losses, ties
+def highscorereading():
+    highscores = {}
+    
